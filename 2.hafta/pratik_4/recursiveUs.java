2fc8989dee1beb87293b6980bc0606716ab5f989
Sena Armagan <81299391+senaarmagan@users.noreply.github.com>
2023-06-28 17:56:44 +0300

    Üs Alma

---
+++ 2.hafta/pratik_4/recursiveUs.java
@@ -0,0 +1,25 @@
+package pratik_5;
+
+import java.util.Scanner;
+/**
+taban ve üs değerleri kullanıcıdan alınan üs alma 
+işlemini "Recursive" metot kullanarak yapan program
+*/
+public class recursiveUs {
+    static int UsAlma(int taban, int us){
+        
+        int sonuc = (int) Math.pow(taban,us);
+        return sonuc;
+    }    
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Taban Değerini giriniz: ");
+        int taban=s.nextInt();
+        System.out.print("Üs değerini yazınız: ");
+        int us = s.nextInt();
+        
+        int sonuc= UsAlma(taban,us);
+        System.out.println("Sonuc: " + sonuc );
+    }
+    }
+
